d5bc2bf73a8a8dc935fb1982047611f7e58d0f96
Liam Duffy <liamdanielduffy@gmail.com>
2024-01-01 22:04:46 -0500

    more readable component formatting

--- src/components.clj
+++ src/components.clj
@@ -1,12 +1,24 @@
 (ns components)
 
-(def charset [:meta {:charset " UTF-8 "}])
-(def tailwind-link [:link {:href " ./tailwind.css " :rel " stylesheet "}])
-(def head-tag [:head {:title " Liam Duffy "} charset tailwind-link])
+(def charset
+  [:meta {:charset " UTF-8 "}])
 
-(def test-typography [:article {:class " prose lg:prose-xl "}
-                      [:h1 " Garlic bread!! with cheese What the science tells us "]])
+(def tailwind-link
+  [:link {:href " ./tailwind.css " :rel " stylesheet "}])
 
-(def body-tag [:body test-typography])
+(def head-tag
+  [:head {:title " Liam Duffy "}
+   charset
+   tailwind-link])
 
-(def html-tag [:html {:lang "en" :class "dark"} head-tag body-tag])
+(def test-typography
+  [:article {:class " prose lg:prose-xl "}
+   [:h1 " Garlic bread!! with cheese What the science tells us "]])
+
+(def body-tag
+  [:body test-typography])
+
+(def html-tag
+  [:html {:lang "en" :class "bg-slate"}
+   head-tag
+   body-tag])
