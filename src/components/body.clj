ec2239807c815f412738b20bb90a54a546bc4376
Liam Duffy <liamdanielduffy@gmail.com>
2024-01-14 12:08:27 -0500

    update site theme

--- src/components/body.clj
+++ src/components/body.clj
@@ -6,49 +6,35 @@
 
 (def top-margin "sm:mt-8 mt-4")
 
-(def hello-world
-  [:span {:class "text-4xl sm:text-5xl text-indigo-100"} "Hello, world!"])
-
-(defn paragraph [& content]
-  [:p {:class "text-lg sm:text-xl text-serif mt-8 max-w-sm sm:max-w-lg"} content])
-
 (defn highlight [content color]
   [:span {:class (str "text-" color)} content])
 
+(def title
+  [:span {:class "mt-4 text-5xl text-gray-900 font-garamond max-w-lg"} "My name is" space "Liam Duffy. I'm a software engineer from Brooklyn."])
+
+(defn paragraph [& content]
+  [:p {:class "text-xl sm:text-2xl text-serif mt-8 max-w-sm sm:max-w-lg"} content])
+
 (defn link [content color href]
-  [:a {:target "_blank" :rel "noopener noreferrer" :href href :class (str "border-b-2 border-dotted" space "border-" color space "text-" color)} content])
+  [:a {:target "_blank" :rel "noopener noreferrer" :href href :class (str "border-b-2 border-dotted" space "border-" color space "text-" color space "text-3xl font-chicago whitespace-nowrap")} content])
 
 (defn nowrap [content]
   [:span {:class "whitespace-nowrap"} content])
 
-(def about-me
-  (paragraph "My name is" space
-             (highlight "Liam." "amber-400") space
-             "I'm a" space
-             (highlight "software engineer" "green-400") space
-             "from" space
-             (nowrap "Brooklyn, NY.")))
-
 (def read-my-resume
-  [:a {:href "/resume.pdf" :target "_blank" :rel "noopener noreferrer" :class "mt-8 mb-10 rounded p-2 bg-amber-400 text-amber-900 font-mono justify-self-start"}
+  [:a {:href "/resume.pdf" :target "_blank" :rel "noopener noreferrer" :class "mt-8 mb-10 rounded p-2 bg-amber-400 text-amber-900 justify-self-start"}
    [:span {:class "bg-yellow-200 text-amber-900 rounded p-1 mr-2"}
     "Hiring?"]
    "Read my résumé ->"])
 
-(def career-summary
-  (paragraph "I've built" space
-             (highlight "full-stack web apps" "sky-300") space
-             "for several" space
-             (highlight "early-stage startups." "red-400")))
-
 (def flowcode-summary
-  (paragraph "I was one of the first engineers at" space
-             (link "Flowcode" "purple-400" c/flowcode-url)
+  (paragraph "I was an early engineer at" space
+             (link "Flowcode" "blue-400" c/flowcode-url)
              \, space "a QR-code builder used by most of the Fortune 1000."))
 
 (def socialstar-summary
   (paragraph "Before that, I was on the founding team of" space
-             (link "Social Star" "pink-400" c/social-star-url)
+             (link "Social Star" "green-500" c/social-star-url)
              \, space "a volunteering platform for one of India's largest non-profits."))
 
 (def juggernaut-summary
@@ -57,17 +43,15 @@
              \, space "a Y-Combinator-backed publication for South Asian journalism."))
 
 (def content
-  [:div {:class "px-6 sm:px-8 font-mono text-indigo-100 flex flex-col sm:items-center"}
-   read-my-resume
-   hello-world
-   about-me
-   career-summary
+  [:div {:class "pt-12 px-6 sm:px-8 font-sans text-slate-800 flex flex-col sm:items-center"}
+   [:span {:class "text-5xl"} "🚲🌳🏙️"]
+   [:p {:class "text-7xl font-garamond mt-6"} "Hello, world!"]
+   title
    flowcode-summary
    socialstar-summary
    juggernaut-summary])
 
 (def tag
-  [:body {:class "min-h-screen min-w-screen bg-gradient-to-t from-slate-800 to-slate-900"}
+  [:body {:class "min-h-screen min-w-screen bg-gray-100"}
    nav/gradient
-   nav/name
    content])
--- src/components/nav.clj
+++ src/components/nav.clj
@@ -2,15 +2,10 @@
 
 (def gradient
   [:div
-   [:div {:class "h-2 bg-blue-600 "}]
-   [:div {:class "h-2 bg-blue-700"}]
-   [:div {:class "h-2 bg-blue-800"}]
-   [:div {:class "h-2 bg-blue-900"}]])
+   [:div {:class "h-2 bg-green-500 "}]
+   [:div {:class "h-2 bg-green-400"}]
+   [:div {:class "h-2 bg-green-300"}]
+   [:div {:class "h-2 bg-green-200"}]])
 
-(def name
-  [:div {:class "ml-4 mt-4"}
-   [:a {:href "/" :class "font-mono text-xl sm:text-2xl text-indigo-100 inline-block"}
-    "✨"
-    [:span
-     {:class "italic ml-1 pb-0 border-dotted border-indigo-300 border-b-2"}
-     "Liam Duffy"]]])
+(def resume
+  [:a {:href "/resume.pdf" :class "inline-block font-chicago text-xl border-b-2 border-dotted border-slate-900"} "Resume"])
